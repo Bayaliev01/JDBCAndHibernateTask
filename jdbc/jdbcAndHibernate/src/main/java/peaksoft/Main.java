1b77c2e850580b200673c95962160d2b018a2122
lesson1 <bayaliev07@gmail.com>
2022-10-19 09:51:47 +0300

    commitjdbc

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,23 @@
 package peaksoft;
 
+import peaksoft.service.UserService;
+import peaksoft.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+       // userService.createUsersTable();
+
+//        userService.saveUser("Daniel", "Toktosunov", (byte)40);
+//        userService.saveUser("Atabek", "Dosbaev", (byte) 33);
+//        userService.saveUser("Siymik", "Bayaliev", (byte) 29);
+//        userService.saveUser("Riskeldi", "Osmonaliev", (byte) 41);
+
+//        System.out.println(userService.getAllUsers());
+
+//        userService.cleanUsersTable();
+
+//        userService.dropUsersTable();
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,23 @@
 package peaksoft.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String name = "postgres";
+    private static final String password = "bayaliev01";
+    private static final String url = "jdbc:postgresql://localhost:5432/jdbc";
+    public static Connection connection(){
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url,name,password);
+            System.out.println("connection success");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
